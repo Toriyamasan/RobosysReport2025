c0f87667ff72b1cc19848b56c8864684c2c84976
Toriyama <s24c1136ww@s.chibakoudai.jp>
2025-12-18 15:32:52 +0900

    不正な入力の修正

--- test.bash
+++ test.bash
@@ -12,44 +12,43 @@ res=0
 # 正常な入力のテスト
 
 # 1h 30m
-out=$(echo "1h 30m" | ./sumtime)
+out=$(echo "1h 30m" | ./sumtime | tr -d '\n')
 [ $? = 0 ] || ng "$LINENO"
 [ "${out}" = "1h 30m" ] || ng "$LINENO"
 
 # 45m
-out=$(echo "45m" | ./sumtime)
+out=$(echo "45m" | ./sumtime | tr -d '\n')
 [ $? = 0 ] || ng "$LINENO"
 [ "${out}" = "45m" ] || ng "$LINENO"
 
 # 1.5h
-out=$(echo "1.5h" | ./sumtime)
+out=$(echo "1.5h" | ./sumtime | tr -d '\n')
 [ $? = 0 ] || ng "$LINENO"
 [ "${out}" = "1h 30m" ] || ng "$LINENO"
 
 # 不正な入力のテスト
 
 # 空入力
-out=$(echo "" | ./sumtime)
+out=$(echo "" | ./sumtime | tr -d '\n')
 [ $? = 0 ] || ng "$LINENO"
 [ "${out}" = "0m" ] || ng "$LINENO"
 
 # 数字なし
-out=$(echo "abc" | ./sumtime)
+out=$(echo "abc" | ./sumtime | tr -d '\n')
 [ $? = 0 ] || ng "$LINENO"
 [ "${out}" = "0m" ] || ng "$LINENO"
 
 # 単位が不正
-out=$(echo "10x" | ./sumtime)
+out=$(echo "10x" | ./sumtime | tr -d '\n')
 [ $? = 0 ] || ng "$LINENO"
 [ "${out}" = "0m" ] || ng "$LINENO"
 
-# 負の値（仕様上は無視される想定）
-out=$(echo "-1h" | ./sumtime)
+# 負の値（仕様上は無視される）
+out=$(echo "-1h" | ./sumtime | tr -d '\n')
 [ $? = 0 ] || ng "$LINENO"
 [ "${out}" = "0m" ] || ng "$LINENO"
 
-############################
 
-[ "${res}" = 0 ] && echo ok # for human
+[ "${res}" = 0 ] && echo ok
 exit $res
 
