0bce222c8d8e1712ffc221d14c941a2c25b8ec88
Toriyama <s24c1136ww@s.chibakoudai.jp>
2025-12-18 16:19:31 +0900

    test.bashtoの改良

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash -xv
+#!/bin/bash
 # SPDX-FileCopyrightText: 2025 Toriyamasan
 # SPDX-License-Identifier: GPL-3.0-only
 
@@ -10,34 +10,18 @@ ng() {
 res=0
 
 # --- 正常な入力のテスト ---
-
-# 1h 30m
-out=$(echo "1h 30m" | ./sumtime | tr -d '\n')
+out=$(echo "1h 30m" | ./sumtime)
 [ $? = 0 ] || ng "$LINENO"
 [ "${out}" = "1h 30m" ] || ng "$LINENO"
 
-# 1.5h
-out=$(echo "1.5h" | ./sumtime | tr -d '\n')
-[ $? = 0 ] || ng "$LINENO"
-[ "${out}" = "1h 30m" ] || ng "$LINENO"
-
-# --- 不正な入力のテスト ---
-
-# 空入力 (エラー終了することを期待)
+# --- 不正な入力のテスト（あえて失敗させて exit 1 か確認する） ---
+# 空入力
 echo "" | ./sumtime
 [ $? != 0 ] || ng "$LINENO"
 
-# 数字なし (エラー終了することを期待)
-echo "abc" | ./sumtime
+# 数字や単位がない文字
+echo "あいうえお" | ./sumtime
 [ $? != 0 ] || ng "$LINENO"
 
-# 単位が不正 (エラー終了することを期待)
-echo "10x" | ./sumtime
-[ $? != 0 ] || ng "$LINENO"
-
-# 負の値 (正規表現にマッチせずエラー終了することを期待)
-echo "-1h" | ./sumtime
-[ $? != 0 ] || ng "$LINENO"
-
-[ "${res}" = 0 ] && echo ok 
+[ "${res}" = 0 ] && echo ok
 exit $res
