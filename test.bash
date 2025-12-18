96529fd0f649d964d09879ca18d74e61b15fee1b
Toriyama <s24c1136ww@s.chibakoudai.jp>
2025-12-18 15:56:26 +0900

    test.bashをREADMEの改良

--- test.bash
+++ test.bash
@@ -3,51 +3,41 @@
 # SPDX-License-Identifier: GPL-3.0-only
 
 ng() {
-    echo No.${1} is failed
+    echo "No.${1} failed"
     res=1
 }
 
 res=0
 
-# 正常な入力のテスト
+# --- 正常な入力のテスト ---
 
 # 1h 30m
 out=$(echo "1h 30m" | ./sumtime | tr -d '\n')
 [ $? = 0 ] || ng "$LINENO"
 [ "${out}" = "1h 30m" ] || ng "$LINENO"
 
-# 45m
-out=$(echo "45m" | ./sumtime | tr -d '\n')
-[ $? = 0 ] || ng "$LINENO"
-[ "${out}" = "45m" ] || ng "$LINENO"
-
 # 1.5h
 out=$(echo "1.5h" | ./sumtime | tr -d '\n')
 [ $? = 0 ] || ng "$LINENO"
 [ "${out}" = "1h 30m" ] || ng "$LINENO"
 
-# 不正な入力のテスト
+# --- 不正な入力のテスト ---
 
-# 空入力
-out=$(echo "" | ./sumtime)
+# 空入力 (エラー終了することを期待)
+echo "" | ./sumtime
 [ $? != 0 ] || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
 
-# 数字なし
-out=$(echo "abc" | ./sumtime)
+# 数字なし (エラー終了することを期待)
+echo "abc" | ./sumtime
 [ $? != 0 ] || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
 
-# 単位が不正
-out=$(echo "10x" | ./sumtime)
+# 単位が不正 (エラー終了することを期待)
+echo "10x" | ./sumtime
 [ $? != 0 ] || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
 
-# 負の値
-out=$(echo "-1h" | ./sumtime)
+# 負の値 (正規表現にマッチせずエラー終了することを期待)
+echo "-1h" | ./sumtime
 [ $? != 0 ] || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo ok 
 exit $res
-
